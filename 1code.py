7b4bfbc960733a493fe7f2a987ede7decf807b7e
vishvaanbu2007-crypto <vishvaanbu2007@gmail.com>
2026-02-05 23:03:58 +0530

    Code 1

--- 1code.py
+++ 1code.py
@@ -1,1 +1,10 @@
-print('my name is vishva')
+def triple_and(name,age,rela):
+    if(name=='vishva'):
+        if(age==18):
+            if(rela=='single'):
+                print(True)
+
+name=input("enter name").lower().strip()
+age=int(input("enter age: "))
+rela=input("enter single or comitted ").lower().strip()
+triple_and(name,age,rela)
