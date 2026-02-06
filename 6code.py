9b26f7891a78bccf8c6ef922d3e7dca973403fe8
vishvaanbu2007-crypto <vishvaanbu2007@gmail.com>
2026-02-06 11:25:29 +0530

    6th code

---
+++ 6code.py
@@ -0,0 +1,15 @@
+def is_anagram(s1, s2):
+    if len(s1) != len(s2):
+        return False
+
+    freq = {}
+
+    for ch in s1:
+        freq[ch] = freq.get(ch, 0) + 1
+
+    for ch in s2:
+        if ch not in freq or freq[ch] == 0:
+            return False
+        freq[ch] -= 1
+
+    return True
