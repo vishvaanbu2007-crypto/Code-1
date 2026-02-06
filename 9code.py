50c3ee40a5294068fa01555f53762565adaf6253
vishvaanbu2007-crypto <vishvaanbu2007@gmail.com>
2026-02-06 11:28:54 +0530

    9th code

---
+++ 9code.py
@@ -0,0 +1,11 @@
+def caesar_cipher(text, shift):
+    result = ""
+
+    for ch in text:
+        if ch.isalpha():
+            base = ord('A') if ch.isupper() else ord('a')
+            result += chr((ord(ch) - base + shift) % 26 + base)
+        else:
+            result += ch
+
+    return result
